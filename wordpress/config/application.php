21616b6ff31592fa58777ce73aa608662cc83786
Web <syed.yousuf@navana.com>
2023-03-15 15:30:54 +0600

    Successfully installed wordpress on local env

---
+++ wordpress/config/application.php
@@ -0,0 +1,37 @@
+<?php
+
+    $host = $_SERVER['WP_HOST'];
+    $protocol = $_SERVER['PROTOCOL'] = isset($_SERVER['HTTPS']) && !empty($_SERVER['HTTPS']) ? 'https' : 'http';
+
+    $full_root_path = '/var/www/html';
+
+    if (getenv('WP_SSL')) {
+        $_SERVER['HTTPS'] = 1;
+        $protocol = 'https';
+    }
+
+    $url = "$protocol://$host";
+    define('WP_HOME', $url);
+    define('WP_SITEURL', "$url/");
+
+    define('WP_CONTENT_DIR', "$full_root_path/wp-content");
+    define('WP_CONTENT_URL', "$url/wp-content");
+
+    define( 'DB_NAME', getenv('MYSQL_DATABASE') );
+
+    /** Database username */
+    define( 'DB_USER', getenv('MYSQL_USER') );
+
+    /** Database password */
+    define( 'DB_PASSWORD', getenv('MYSQL_PASSWORD') );
+
+    /** Database hostname */
+    define( 'DB_HOST', getenv('DB_HOST') );
+
+    /** Database charset to use in creating database tables. */
+    define( 'DB_CHARSET', getenv('DB_CHARSET') );
+
+    /** The database collate type. Don't change this if in doubt. */
+    define( 'DB_COLLATE', '' );
+
+    $table_prefix = 'wp_';
--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -18,60 +18,10 @@
  * @package WordPress
  */
 
-// ** Database settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
+require_once(dirname(__FILE__) . '/config/application.php');
 
-$DB_NAME = getenv('MYSQL_DATABASE');
-$DB_USER = getenv('MYSQL_USER');
-$DB_HOST = getenv('DB_HOST');
-$DB_PASSWORD = getenv('MYSQL_PASSWORD');
+define( 'FS_METHOD', 'direct' );
 
-define( 'DB_NAME', $DB_NAME );
-
-/** Database username */
-define( 'DB_USER', $DB_USER );
-
-/** Database password */
-define( 'DB_PASSWORD', $DB_PASSWORD );
-
-/** Database hostname */
-define( 'DB_HOST', $DB_HOST );
-
-/** Database charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8mb4' );
-
-/** The database collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', '' );
-
-/**#@+
- * Authentication unique keys and salts.
- *
- * Change these to different unique phrases! You can generate these using
- * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
- *
- * You can change these at any point in time to invalidate all existing cookies.
- * This will force all users to have to log in again.
- *
- * @since 2.6.0
- */
-define( 'AUTH_KEY',         '&~lqD/sLEQ27/iPrFACie1}X|N=_Pvq4-&26aA7B}X9<<sdrl&BwLU3qRA1T?B$4' );
-define( 'SECURE_AUTH_KEY',  'qt sDV9C|[-hZQwyb}.kQHX6r4YrTCy>Qg};CVoRF&vN@//d|9q~Tj#.nSkwF)@V' );
-define( 'LOGGED_IN_KEY',    'sr-wU}rva5D]GSFQVubKXn0p-7mCJq1K%s|RD.8fW!jM 3}Na^Mw3OUE^5m=p^fY' );
-define( 'NONCE_KEY',        'O0Y4ET_Vy@Ol-5tN<z:aJ/aI4^|$x<aIvd)2HbuIR-/0(04E%fZaZ=hr3:<1V{Q4' );
-define( 'AUTH_SALT',        'Lk.RYiQ]|bCf~AYIA-ex_9e$hksCONj3 dZizgZrH*43Q`}X:T[hpbkv9eG%UY+e' );
-define( 'SECURE_AUTH_SALT', 'N}YEhzaw2>P71@5femGl[Z.B.T >5Q)c$VpER,P1j&`bs29&Iif1leraW|No0R3A' );
-define( 'LOGGED_IN_SALT',   '&XA__H7,M[iqY.AzVy><L16G)1Ou[]E74wB4|ZA)Lgt;]K~_>=XAYDna)*Af#f8Q' );
-define( 'NONCE_SALT',       '^0eMQylnoGor;z=P1oD{ Is%7>khr>@d%%8Mr=A*nS&6~kUCeV?,r0$ZT^(Ld>$R' );
-
-/**#@-*/
-
-/**
- * WordPress database table prefix.
- *
- * You can have multiple installations in one database if you give each
- * a unique prefix. Only numbers, letters, and underscores please!
- */
-$table_prefix = 'wp_';
 
 /**
  * For developers: WordPress debugging mode.
@@ -85,7 +35,7 @@ $table_prefix = 'wp_';
  *
  * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
-define( 'WP_DEBUG', false );
+define( 'WP_DEBUG', true );
 
 /* Add any custom values between this line and the "stop editing" line. */
 
