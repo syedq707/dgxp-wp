ca4580f4ffe0273cea83136a72c66419e51c3238
Web <syed.yousuf@navana.com>
2023-03-16 17:23:49 +0600

    Theme and styling updated with landing

--- wordpress/wp-admin/admin-header.php
+++ wordpress/wp-admin/admin-header.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * WordPress Administration Template Header
- *
- * @package WordPress
- * @subpackage Administration
- */
+
 
 header( 'Content-Type: ' . get_option( 'html_type' ) . '; charset=' . get_option( 'blog_charset' ) );
 if ( ! defined( 'WP_ADMIN' ) ) {
