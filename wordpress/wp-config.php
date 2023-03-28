469049691f1968f1b0c22da2a14617bf0f591893
Web <syed.yousuf@navana.com>
2023-03-28 13:10:21 +0600

    wp-config updated with url rediredtion

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -18,12 +18,12 @@
  * @package WordPress
  */
 
- $url = "https://digitalgxp.com/blog/";
- define('WP_HOME', "https://digitalgxp.com/blog");
- define('WP_SITEURL', "https://digitalgxp.com/blog");
+$url = "https://digitalgxp.com/blog/";
+define('WP_HOME', "https://digitalgxp.com/blog");
+define('WP_SITEURL', "https://digitalgxp.com/blog");
 
- // Rewrite your access URL to the admin page
-// $_SERVER['REQUEST_URI'] = str_replace("/wp-admin/", "/blog/wp-admin/",  $_SERVER['REQUEST_URI']);
+// Rewrite your access URL to the admin page
+$_SERVER['REQUEST_URI'] = str_replace("/wp-admin/", "/blog/wp-admin/",  $_SERVER['REQUEST_URI']);
 
 
 // ** Database settings - You can get this info from your web host ** //
